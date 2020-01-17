e42a4c9599b6fb80023944d679e16adde8bbc4e4
Meghana Kanagal <meghanaskanagal@gmail.com>
2020-01-17 21:37:51 +0530

    adding my refactored code

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,20 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import React from "react";
 import "@patternfly/react-core/dist/styles/base.css";
-import { Page, PageHeader } from "@patternfly/react-core";
+import Header from "./components/header/header";
+import Search from "./components/search/search"
+import SearchResult from "./components/searchResult/searchResult";
 
-import SearchData from "./types";
-import SearchForm from "./Search";
-import DisplayList from "./Display";
-
-import Homepage from "./Homepage";
-
-const logoProps = {
-  href: "https://erdemo.io",
-  target: "_blank"
-};
-
-const Header = (
-  <PageHeader
-    logo="Find My Relative"
-    logoProps={logoProps}
-    toolbar="Toolbar"
-    avatar=" | Avatar"
-  />
-);
-
-const OldApplication: React.FC = () => {
-  const [victimList, setVictimList] = useState([]);
-  const [isDataReady, setIsDataReady] = useState(true);
-  const [isResponseOk, setIsResponseOk] = useState(true);
-
-  const fetchDetails = async (data: SearchData) => {
-    setIsDataReady(false);
-    console.log(
-      "sending request to: ",
-      process.env.REACT_APP_BACKEND_URL + `/find/victim/byName/${data.name}`
-    );
-    const response = await fetch(
-      process.env.REACT_APP_BACKEND_URL + `/find/victim/byName/${data.name}`
-    );
-
-    if (response.ok) {
-      const result = await response.json();
-      setVictimList(result.map.victims.list);
-      setIsResponseOk(true);
-    } else if (response.status === 503) {
-      setVictimList([]);
-      setIsResponseOk(false);
-      console.log("service unavailable.");
-    }
-    setIsDataReady(true);
-  };
-
-  return (
-    <Page header={Header}>
-      <SearchForm onFormSubmit={fetchDetails}></SearchForm>
-      <DisplayList
-        responseOk={isResponseOk}
-        isReady={isDataReady}
-        dataArray={victimList}
-      ></DisplayList>
-    </Page>
-  );
-};
 
 const App: React.FC = () => {
-  return (
-    <Router>
-      <Route path="/" component={OldApplication} />
-      <Route path="/newfindmyrelative" component={Homepage} />
-    </Router>
-  );
+    // const [victimList, setVictimList] = useState();
+
+    return (
+        <div>
+            <Header />
+            <Search />
+            <SearchResult />
+        </div>
+    );
 };
 
 export default App;
